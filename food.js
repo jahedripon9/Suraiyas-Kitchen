708f54cc2874e88fbafc04292c9328eedc76014c
jahedripon9 <jahedahmed3170@gmail.com>
2021-08-31 01:39:55 +0600

    first commit

--- food.js
+++ food.js
@@ -35,8 +35,8 @@ const displaySearchResults = meals => {
         <div onclick="loadMealDetail(${meal.idMeal})" class="card h-50 ">
         <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${meal.strMeal}</h5>
-          <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+          <h5 class="card-title ">${meal.strMeal}</h5>
+          <p class="card-text ">${meal.strInstructions.slice(0, 200)}</p>
         </div>
       </div>
         `
@@ -63,8 +63,8 @@ const displayMealDetail = meal => {
     div.innerHTML = `
     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title text-light">${meal.strMeal}</h5>
-          <p class="card-text bg-white">${meal.strInstructions.slice(0, 120)}</p>
+          <h5 class="card-title text-black">${meal.strMeal}</h5>
+          <p class="card-text text-black ">${meal.strInstructions.slice(0, 120)}</p>
           <a href="${meal.strYoutube }" class="btn btn-primary">Go somewhere</a>
         </div> 
     `
